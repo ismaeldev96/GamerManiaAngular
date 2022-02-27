feb9809d14b1d34e54af3527d1f1fb3e1be609bf
Ismael <ismaeldev96@gmail.com>
2022-02-27 14:00:08 -0300

    criando as paginas no angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './views/template/home/home.component';
+import { LoginComponent } from './views/pages/login/login.component';
+import { CarrinhoComponent } from './views/pages/carrinho/carrinho.component';
+import { UsuarioComponent } from './views/pages/usuario/usuario.component';
+import { ProdutoComponent } from './views/pages/produto/produto.component';
+import { BuscaComponent } from './views/pages/busca/busca.component';
+import { CategoriaComponent } from './views/pages/categoria/categoria.component';
+import { FavoritoComponent } from './views/pages/favorito/favorito.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: HomeComponent},
+  {path: "page/carrinho", component: CarrinhoComponent},
+  {path: "page/usuario", component: UsuarioComponent},
+  {path: "page/contato", component: CarrinhoComponent},
+  {path: "page/produto", component: ProdutoComponent},
+  {path: "page/categoria", component: CategoriaComponent},
+  {path: "page/pesquisar", component: BuscaComponent},
+  {path: "page/favorito", component: FavoritoComponent},
+  {path: "page/login", component: LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './views/template/header/header.component';
+import { FooterComponent } from './views/template/footer/footer.component';
+import { HomeComponent } from './views/template/home/home.component';
+import { LoginComponent } from './views/pages/login/login.component';
+import { CarrinhoComponent } from './views/pages/carrinho/carrinho.component';
+import { ProdutoComponent } from './views/pages/produto/produto.component';
+import { CategoriaComponent } from './views/pages/categoria/categoria.component';
+import { BuscaComponent } from './views/pages/busca/busca.component';
+import { ContatoComponent } from './views/pages/contato/contato.component';
+import { UsuarioComponent } from './views/pages/usuario/usuario.component';
+import { FavoritoComponent } from './views/pages/favorito/favorito.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    LoginComponent,
+    CarrinhoComponent,
+    ProdutoComponent,
+    CategoriaComponent,
+    BuscaComponent,
+    ContatoComponent,
+    UsuarioComponent,
+    FavoritoComponent
   ],
   imports: [
     BrowserModule,
