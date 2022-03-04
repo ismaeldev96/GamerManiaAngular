605f3b9c876d622cdaf8b743204365de0c98ed90
Ismael <ismaeldev96@gmail.com>
2022-03-04 13:36:02 -0300

    atualização pagina de login

--- src/app/views/pages/login/login.component.ts
+++ src/app/views/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-login',
@@ -12,4 +13,9 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  loginModel = new User();
+
+  onSubmit(){
+    console.log(this.loginModel);
+  }
 }
