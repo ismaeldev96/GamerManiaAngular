98564d58cccd06c7857fdf1d529b519226f5a6be
Ismael <ismaeldev96@gmail.com>
2022-03-13 18:51:46 -0300

    sistema para efetuar o login

--- src/app/views/pages/login/login.component.ts
+++ src/app/views/pages/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { User } from 'src/app/models/user';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-login',
@@ -8,14 +10,28 @@ import { User } from 'src/app/models/user';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private loginService: LoginService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
   loginModel = new User();
+  
+  mensagem = "";
+  contador = 0;
 
   onSubmit(){
-    console.log(this.loginModel);
+    if(this.contador < 3){
+      this.loginService.login(this.loginModel).subscribe((response) => {
+        this.router.navigateByUrl('')
+      }, (respostaErro) => {
+        this.mensagem = respostaErro.error;
+        alert(this.mensagem)
+        this.contador++
+      })
+    }else{
+      this.mensagem = "Tentativas esgotadas"
+      alert(this.mensagem)
+    }
   }
 }
